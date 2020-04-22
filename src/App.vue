adcd679c510a39cc0d28eca8302ba0400e676d11
Craig <Family@study-mac.home>
2020-04-22 12:56:02 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,69 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <main :class=" getTime() > 11 ? 'night' : ''">
+   <div class="search-box">
+     <input type="text" 
+     name="search" 
+     class="search-bar" 
+     placeholder="Enter a place..."
+     v-model="query"
+     v-on:keypress="getWeather" />
+   </div>
+   <div class="weather-holder" v-if="typeof weather.main != 'undefined'">
+     <div class="location"> {{weather.name}} </div>
+     <div class="date"> {{dateBuilder()}}</div>
+     <div class="temperature">{{Math.round(weather.main.temp)}}°C</div>
+    <div class="temperature-sub">
+      Min.{{Math.round(weather.main.temp_min)}}°C 
+      | 
+      Max.{{Math.round(weather.main.temp_max)}}°C
+    </div>
+  <div class="conditions">{{weather.weather[0].description}}</div>
+  <!-- <div>{{weather.weather[0].id}}</div> -->
+  </div>
+   </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'app',
+  data () {
+    return {
+      api_key: '60d92afe4a38cfd33fb59984d761c987',
+      url_base: 'https://api.openweathermap.org/data/2.5/',
+      query: '',
+      weather: {}
+    }
+  },
+  methods: {
+    getWeather (e) {
+      if (e.key == "Enter") {
+        fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+          .then(res => {
+            return res.json();
+          }).then(this.setResults);
+      }
+    },
+    setResults (results) {
+      this.weather = results;
+    },
+    dateBuilder () {
+      let d = new Date();
+      let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+      return `${day} ${date} ${month} ${year}`;
+    },
+    getTime () {
+      let d = new Date()
+      let h = d.getHours()
+      return h
+
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
